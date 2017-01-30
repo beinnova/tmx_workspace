1418371963d2195d2dabfd692b9f67fc2e459097
Giorgio Cerruti <g.cerruti@travelmatic.com>
2017-01-30 09:10:06 +0100

    Initial commit

---
+++ launch-tmux-workspace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WORKSPACE="workspace"
+WORKDIR=/home/giorgio/workspace
+
+tmux new -d -s $WORKSPACE
+
+tmux send-keys -t $WORKSPACE:0 'vim' Enter
+tmux new-window -a -c $WORKDIR -t $WORKSPACE
+tmux new-window -a -n "lab" -c $WORKDIR -t $WORKSPACE
+tmux split-window -c $WORKDIR -h 
+tmux split-window -c $WORKDIR -v 
+
+tmux select-window -t $WORKSPACE:0
+tmux -2 attach-session -t $WORKSPACE
